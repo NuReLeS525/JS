e1ec805fe28f7a0a78bc5bf592c9e0a673abfe9d
NuReLeS525 <122634975+NuReLeS525@users.noreply.github.com>
2023-08-09 12:34:56 +0300

    Add files via upload

---
+++ lesson80/main.js
@@ -0,0 +1,20 @@
+for(let i = 2; i <= 12; i = i + 2) {
+  console.log(i)
+}
+
+for(let i = 10; i >= 1; i --) {
+  console.log(i)
+}
+
+
+let i = 2
+while(i <= 12) {
+  console.log(i)
+  i = i + 2
+}
+
+i = 10
+while(i >= 1) {
+  console.log(i)
+  i --
+}
