ad1fb531108363d05bf9189f506672ac2f550bb0
NuReLeS525 <122634975+NuReLeS525@users.noreply.github.com>
2023-08-09 10:14:56 +0300

    Add files via upload

---
+++ lesson76/main.js
@@ -0,0 +1,8 @@
+const firstName = 'Nureles'
+const lastName = 'Tashbolotov'
+const patronymic = 'Aslanbekovich'
+let age = 15
+const sex = 'male'
+console.log(firstName, lastName, patronymic)
+console.log(age)
+console.log(sex)
