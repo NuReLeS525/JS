84b07154ab1c16026d3f02f3e6991e3405849577
NuReLeS525 <122634975+NuReLeS525@users.noreply.github.com>
2023-08-14 12:04:37 +0300

    Add files via upload

---
+++ lesson81/main.js
@@ -0,0 +1,13 @@
+const numbers = [39, 20, 106, 11, 94]
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 == 0) {
+    console.log(numbers[i])
+  }
+}
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+for (let i = rainbow.length - 1; i >= 0; i--) {
+  console.log(rainbow[i])
+}
